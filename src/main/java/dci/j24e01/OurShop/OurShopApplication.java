2a2f3613389a8b33be9186c17d0c481d141533b0
Helder Pereira <phelder@gmail.com>
2024-10-29 09:40:54 +0100

    First commit

---
+++ src/main/java/dci/j24e01/OurShop/OurShopApplication.java
@@ -0,0 +1,13 @@
+package dci.j24e01.OurShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dci/j24e01/OurShop/OurShopApplicationTests.java
@@ -0,0 +1,13 @@
+package dci.j24e01.OurShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
