a1d9cb34f6a39dfe3d4ce3a2b028a275ab7bd26b
Herr Oleh Bryl <olegbryl@gmail.com>
2024-11-04 15:57:14 +0100

    merging

--- src/main/java/dci/j24e01/OurShop/controllers/AppController.java
+++ src/main/java/dci/j24e01/OurShop/controllers/AppController.java
@@ -1,7 +1,11 @@
 package dci.j24e01.OurShop.controllers;
 
+import dci.j24e01.OurShop.models.NavLink;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
 
 @Controller
 public class AppController {
@@ -9,5 +13,14 @@ public class AppController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @ModelAttribute("navLinks")
+    public List<NavLink> getNavLinks(){
+        return List.of(
+                new NavLink("Home", "/"),
+                new NavLink("Categories", "/categories"),
+                new NavLink("Products", "/products")
+        );
     }
 }
