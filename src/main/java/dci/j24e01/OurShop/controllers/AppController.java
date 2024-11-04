7540c411d7e25d93e1fa6796b802454437c024c6
Herr Oleh Bryl <olegbryl@gmail.com>
2024-11-04 13:50:12 +0100

    added index.html and AppController

---
+++ src/main/java/dci/j24e01/OurShop/controllers/AppController.java
@@ -0,0 +1,13 @@
+package dci.j24e01.OurShop.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
