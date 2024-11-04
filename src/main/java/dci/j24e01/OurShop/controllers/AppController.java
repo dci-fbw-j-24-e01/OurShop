4a9f82a0677c00576e9dbac193a340bd8fe1b12a
Herr Oleh Bryl <olegbryl@gmail.com>
2024-11-04 16:07:25 +0100

    bugs fixed. The merging of Robin's work and mine has done well.

--- src/main/java/dci/j24e01/OurShop/controllers/AppController.java
+++ src/main/java/dci/j24e01/OurShop/controllers/AppController.java
@@ -1,11 +1,10 @@
 package dci.j24e01.OurShop.controllers;
 
-import dci.j24e01.OurShop.models.NavLink;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
-import java.util.List;
+
+
 
 @Controller
 public class AppController {
